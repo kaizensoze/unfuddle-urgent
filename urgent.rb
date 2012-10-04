e39420fe0f81bfeb63dd7ceaa320c3efb15b2ed6
Joe Gallo <gallo.j@gmail.com>
2012-10-04 01:00:37 -0400

    Initial commit.

---
+++ urgent.rb
@@ -0,0 +1,38 @@
+#!/usr/bin/ruby
+
+require 'curb'
+require 'date'
+require 'json'
+
+def check_tickets()
+  url = "https://elephantventures.unfuddle.com/api/v1/projects/1042/tickets?format=json"
+
+  c = Curl::Easy.new(url)
+  c.http_auth_types = :basic
+  c.username = 'XXXXX'
+  c.password = 'XXXXX'
+  c.perform
+
+  ticket_data = c.body_str
+  # puts JSON.pretty_generate(ticket)
+  tickets = JSON.parse(ticket_data)
+
+  tickets.each do |ticket|
+    ticket_created_at = ticket['created_at']
+    ticket_datetime = DateTime.parse(ticket_created_at)
+
+    time_ago = DateTime.now - ticket_datetime
+    time_ago_in_minutes = (time_ago * 24 * 60).to_i
+    
+    if time_ago_in_minutes < 5000  # or whatever you set the crontab at
+      if ticket['priority'].to_i >= 4
+        `afplay foreigner_urgent_part.mp3`
+        break
+      end
+    end
+  end
+end
+
+if __FILE__ == $0
+  check_tickets()
+end
